10af88705c559eef78db010206f8da0c079c4645
haiderali780 <muhammadhaiderali782002@gmail.com>
2023-01-11 23:39:34 -0800

    react-to-lost-focus

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -14,6 +14,15 @@ const SimpleInput = (props) => {
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
+  
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
@@ -49,6 +58,7 @@ const SimpleInput = (props) => {
           type='text'
           id='name'
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
          {nameInputIsInvalid && (
